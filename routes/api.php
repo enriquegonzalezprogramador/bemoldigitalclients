ab671ec58b1c76e22a5013f2e58a963fe3c732dc
Junior Enrique <twd00027@gmail.com>
2021-09-17 18:34:39 -0300

    33dd4444324242d

--- routes/api.php
+++ routes/api.php
@@ -10,13 +10,13 @@ Route::group([
     'middleware' => 'api',
     'prefix' => 'auth'
 ], function ($router) {
-    secure_url(Route::post('login', 'Auth\Controllers\AuthController@login'));
-    secure_url(Route::post('login-app', 'Auth\Controllers\AuthController@loginApp'));
-    secure_url(Route::post('register', 'Auth\Controllers\AuthController@register'));
-    secure_url(Route::post('logout', 'Auth\Controllers\AuthController@logout'));
-    secure_url(Route::post('refresh', 'Auth\Controllers\AuthController@refresh'));
-    secure_url(Route::post('user', 'Auth\Controllers\AuthController@user'));
-    secure_url(Route::post('recover-password/{email}', 'Auth\Controllers\AuthController@forgotPassword'));
+    secure_url(Route::post('login', 'Auth\Controllers\AuthController@login',[], false));
+    secure_url(Route::post('login-app', 'Auth\Controllers\AuthController@loginApp',[], false));
+    secure_url(Route::post('register', 'Auth\Controllers\AuthController@register',[], false));
+    secure_url(Route::post('logout', 'Auth\Controllers\AuthController@logout',[], false));
+    secure_url(Route::post('refresh', 'Auth\Controllers\AuthController@refresh',[], false));
+    secure_url(Route::post('user', 'Auth\Controllers\AuthController@user',[], false));
+    secure_url(Route::post('recover-password/{email}', 'Auth\Controllers\AuthController@forgotPassword',[], false));
 }); 
 
 
@@ -28,7 +28,7 @@ Route::group([
 
     secure_url(Route::resource('user', 'User\Controllers\UserController', [
         'only' => $defaultResources
-    ]));
+    ],[], false));
 
     /*Route::resource('employee', 'EmployeeController', [
         'only' => $defaultResources
